567ad56b474e9bb5149165bbcb41e760a70371fc
lany <lany192@qq.com>
2017-12-01 22:04:25 +0800

    修改用户

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from user.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_time', 'modified_time', 'age', 'nickname', 'birthday']
+    list_per_page = 10
+    search_fields = ['name', ]
+    list_editable = ['age', ]
+    list_filter = ['created_time', ]
+    # 设置哪些字段可以点击进入编辑界面
+    list_display_links = ('id', 'name')
+
+
+admin.site.register(User, UserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.six import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class User(models.Model):
+    name = models.CharField('用户姓名', max_length=100)
+    age = models.IntegerField('用户年龄', default=1)
+    nickname = models.CharField('昵称', max_length=256)
+    birthday = models.DateField('生日', default=timezone.now)
+    created_time = models.DateTimeField('创建日期', default=timezone.now)
+    modified_time = models.DateTimeField('最后修改日期', auto_now=True)
+
+
+def __str__(self):
+    return self.name
