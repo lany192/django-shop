6fcb468fbb1607e88866b85aa4a3f6413abe8451
Lany <lany168@qq.com>
2017-12-02 10:51:55 +0800

    update

--- blog/serializers.py
+++ blog/serializers.py
@@ -7,7 +7,7 @@ from blog.models import Category, Tag, Post
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('id', 'url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
