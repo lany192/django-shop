1f882b841b43400eb24c53947dfd32075f73b87b
lany <lany192@qq.com>
2017-12-04 00:49:18 +0800

    添加国家省份城市地区

--- order_system/admin.py
+++ order_system/admin.py
@@ -1,3 +1,35 @@
+# coding:utf-8
 from django.contrib import admin
 
-# Register your models here.
+from order_system.models import Area, City, Province, Address, Order
+
+
+class AppTokenAdmin(admin.ModelAdmin):
+    list_display = ['user', 'token', 'created_time', 'overtime']
+
+
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'postcode']
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'postcode']
+
+
+class ProvinceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'detail', 'consignee', 'phone']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_no', 'user', 'address', 'created_time']
+
+
+admin.site.register(Area, AreaAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Province, ProvinceAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Order, OrderAdmin)
--- order_system/models.py
+++ order_system/models.py
@@ -1,25 +1,92 @@
+from datetime import timezone
+
 from django.db import models
-from django.utils import timezone
 from django.utils.six import python_2_unicode_compatible
 
-from system.storage import CustomFileStorage
+from goods.models import Goods
 from user.models import UserProfile
+
+
+@python_2_unicode_compatible
+class Area(models.Model):  # 地区
+    name = models.CharField('地区名称', max_length=64)
+    postcode = models.IntegerField('邮编', null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "地区"
+        verbose_name_plural = "地区"
+
+
+@python_2_unicode_compatible
+class City(models.Model):  # 城市
+    name = models.CharField('城市名称', max_length=64)
+    area = models.ManyToManyField(Area, verbose_name="地区")
+    postcode = models.IntegerField('邮编', null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "城市"
+        verbose_name_plural = "城市"
+
+
+@python_2_unicode_compatible
+class Province(models.Model):  # 省份
+    name = models.CharField('省份名称', max_length=64)
+    city = models.ManyToManyField(City, verbose_name="城市")
+
+    def __str__(self):
+        return self.country_name
+
+    class Meta:
+        verbose_name = "省份"
+        verbose_name_plural = "省份"
+
+
+@python_2_unicode_compatible
+class Country(models.Model):  # 国家
+    name = models.CharField('国家/地区名称', max_length=64)
+    province = models.ManyToManyField(Province, verbose_name="省份")
+
+    def __str__(self):
+        return self.country_name
+
+    class Meta:
+        verbose_name = "国家"
+        verbose_name_plural = "国家"
+
+
+@python_2_unicode_compatible
+class Address(models.Model):  # 收货地址
+    user = models.OneToOneField(UserProfile, verbose_name='用户')
+    country = models.ManyToManyField(Country, verbose_name='国家')
+    detail = models.CharField('详细地址', max_length=512)
+    consignee = models.CharField('收件人', max_length=32)
+    phone = models.CharField('电话', max_length=32)
+
+    def __str__(self):
+        return self.detail
+
+    class Meta:
+        verbose_name = "收货地址"
+        verbose_name_plural = "收货地址"
 
 
 @python_2_unicode_compatible
 class Order(models.Model):  # 订单实体
     order_no = models.CharField('订单编号', max_length=64)
-    nickname = models.CharField('昵称', max_length=64)
-    birthday = models.DateField('生日', default=timezone.now)
-    email = models.EmailField('邮箱', max_length=32)
-    phone = models.CharField('电话', max_length=32)
-    password = models.CharField('登录密码', max_length=64)
-    avatar = models.ImageField('用户头像', upload_to='avatar/', default='avatar/user0.jpg', storage=CustomFileStorage())
-    signature = models.CharField(max_length=128, default='这家伙很懒，什么也没留下.')
-    created_time = models.DateTimeField('创建日期', default=timezone.now)
-    modified_time = models.DateTimeField('最后修改日期', auto_now=True)
-    delete_flag = models.BooleanField('删除标记', default=False)
+    goods = models.ManyToManyField(Goods, verbose_name='购买的商品')
     user = models.ForeignKey(UserProfile, verbose_name='下单用户')
+    address = models.ForeignKey(Address, verbose_name='收货地址')
+    created_time = models.DateTimeField('创建日期', default=timezone.now)
 
     def __str__(self):
-        return self.name
+        return self.order_no
+
+    class Meta:
+        verbose_name = "订单"
+        verbose_name_plural = "订单"
